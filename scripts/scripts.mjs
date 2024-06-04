d36ddcb9c825ff2ba8b0a2370ba9428c20259793
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-03 21:09:36 -0700

    Added MBTI to Pokemon function
    Added functionality to output Pokémon depending on user's MBTI

--- scripts/scripts.mjs
+++ scripts/scripts.mjs
@@ -5,10 +5,12 @@ let btn = document.querySelector("button")
 // console.log(btn);
 let dropForm = document.querySelector("form");
 // console.log(dropForm);
-const mbtiList = {"INTJ": "field", "INTP": "field", "ENTJ": "dragon", "ENTP": "humanoid", 
-                  "INFJ": "grass", "INFP": "fairy", "ENFJ": "humanoid", "ENFP": "ditto", 
+let submitBtn;
+let mbtiValue;
+const mbtiList = {"INTJ": "ground", "INTP": "ground", "ENTJ": "dragon", "ENTP": "humanshape", 
+                  "INFJ": "plant", "INFP": "fairy", "ENFJ": "humanshape", "ENFP": "ditto", 
                   "ISTJ": "water2", "ISFJ": "mineral", "ESTJ": "water1", "ESFJ": "flying", 
-                  "ISTP": "amorphous", "ISFP": "bug", "ESTP": "water3", "ESFP": "monster", 
+                  "ISTP": "indeterminate", "ISFP": "bug", "ESTP": "water3", "ESFP": "monster", 
 };
 
 btn.addEventListener("click", pTypePage);
@@ -19,8 +21,14 @@ function pTypePage() {
     displayBox[0].childNodes[3].textContent = 
         "Please select your personlity from the dropdown menu.";
     createListOptions();
-    submitBtn();
+    createSubmitBtn();
     btn.style.display = "none";
+
+    submitBtn = document.querySelector("button");
+    console.log(submitBtn);
+    submitBtn.addEventListener("click", eggDisplay); 
+    // const apiData = await pokemonAPI();
+    // console.log(apiData);
 }
 
 function createListOptions() {
@@ -41,15 +49,65 @@ function createListOptions() {
         optionSelectEl.appendChild(optionEl);
     };
 
-    console.log(dropForm);
+    // console.log(dropForm);
 }
 
-function submitBtn() {
-    submitBtn = document.createElement("input");
+function createSubmitBtn() {
+    submitBtn = document.createElement("button");
     submitBtn.setAttribute("type", "submit");
-    submitBtn.setAttribute("value", "Submit");
-    console.log(submitBtn);
-    const breakLine = document.createElement("br");
-    dropForm.appendChild(breakLine);
-    dropForm.appendChild(submitBtn);
+    // submitBtn.setAttribute("value", "Submit");
+    submitBtn.setAttribute("form", "myForm")
+    submitBtn.innerText = "Submit";
+    // console.log(submitBtn);
+    // const breakLine = document.createElement("br");
+    // dropForm.appendChild(breakLine);
+    const parentClass = dropForm.parentNode;
+    parentClass.appendChild(submitBtn)
+    parentClass.replaceChild(submitBtn, btn);
 }
+
+async function eggDisplay(e) {
+    // console.log("Egg")
+    e.preventDefault();
+    // console.log(e.target.form.elements.mbti.value);
+    mbtiValue = e.target.form.elements.mbti.value;
+    // console.log(mbtiValue);
+    // return mbtiValue;
+    const apiData = await pokemonAPI();
+    // console.log(apiData);
+    const eggGroup = mbtiList[mbtiValue];
+    // console.log(eggGroup);
+    apiData.forEach(async (eggs) => {
+        // console.log(eggs.name)
+        if(eggs.name == eggGroup)
+        {
+            // console.log(eggs)
+            // console.log(eggs.url);
+            const eggGroupAPI = await fetch(eggs.url);
+            const pokemonArr = await eggGroupAPI.json();
+            // console.log(pokemon.pokemon_species.length);
+            const numOfPokemon = pokemonArr.pokemon_species.length;
+            const randomNum = getRNG(numOfPokemon);
+            const pokemon = pokemonArr.pokemon_species[randomNum].name;
+            // console.log(pokemon);
+        }
+    });
+}
+
+async function pokemonAPI() {
+    const apiResult = await fetch("https://pokeapi.co/api/v2/egg-group");
+    // console.log(apiResult);
+    const data = await apiResult.json();
+    // console.log(data);
+    const results = await data.results;
+    // console.log(results)
+    return results;
+}
+
+function getRNG(maxNum) {
+    return Math.floor(Math.random() * maxNum);
+}
+
+// const pokemon = https://pokeapi.co/api/v2/pokemon/ + ditto
+// pokemon.sprites.front_default
+// imbed to <img>
